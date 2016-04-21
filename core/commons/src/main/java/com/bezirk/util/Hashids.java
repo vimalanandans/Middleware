a29db7b4597c2586c9bad86e3550c51f0135ee2c
Rishabh <rishabhsgulati@gmail.com>
2016-04-21 11:50:36 -0400

    fixing import issues, removing unused init.gradle and reference from settings.gradle, todo clean readme.md

--- core/commons/src/main/java/com/bezirk/util/Hashids.java
+++ core/commons/src/main/java/com/bezirk/util/Hashids.java
@@ -1,5 +1,7 @@
 package com.bezirk.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
--- java/libraries/comms-zyre-jni/src/test/java/org/zyre/Compressor.java
+++ java/libraries/comms-zyre-jni/src/test/java/org/zyre/Compressor.java
@@ -1,6 +1,14 @@
 package org.zyre;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.DeflaterOutputStream;
+import java.util.zip.InflaterInputStream;
+
 public class Compressor {
 
     public static byte[] compress(String text) {
