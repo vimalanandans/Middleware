f42b67ed3888f3e065f74a7c09dd4ff6336bc4ff
Michael Maass <mam1pi@bosch.com>
2016-04-22 09:38:09 -0400

    Documented UnicastStream to close #23

--- core/bezirk-middleware-api/src/main/java/com/bezirk/middleware/messages/UnicastStream.java
+++ core/bezirk-middleware-api/src/main/java/com/bezirk/middleware/messages/UnicastStream.java
@@ -15,16 +15,34 @@ package com.bezirk.middleware.messages;
 import com.bezirk.middleware.addressing.ServiceEndPoint;
 
 /**
+ * Base class for streams with one recipient. Extend {@link MulticastStream} for streams that
+ * have more than one recipient.
+ *
+ * @see Stream
+ * @see MulticastStream
+ * @see com.bezirk.middleware.addressing.ServiceEndPoint
  */
 public abstract class UnicastStream extends Stream {
-
     private final ServiceEndPoint recipient;
 
+    /**
+     * Create a <code>Stream</code> with one recipient where the recipient is in one of the sender's
+     * spheres.
+     *
+     * @param flag      flag to mark the intent of this stream
+     * @param topic     the pub-sub topic for this stream
+     * @param recipient the specific component that should receive this stream
+     */
     public UnicastStream(Flag flag, String topic, ServiceEndPoint recipient) {
         super(flag, topic);
         this.recipient = recipient;
     }
 
+    /**
+     * Returns the endpoint that is the recipient of this stream.
+     *
+     * @return the endpoint that is the recipient of this stream
+     */
     public ServiceEndPoint getRecipient() {
         return recipient;
     }
