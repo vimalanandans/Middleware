f737d86021fd1db2473494dcc4fe29843fb36475
Michael Maass <mam1pi@bosch.com>
2016-06-06 12:41:37 -0400

    Updated TestApp to test more Bezirk features

--- java/testApp/src/main/java/test/Test.java
+++ java/testApp/src/main/java/test/Test.java
@@ -6,12 +6,14 @@ package test;
 import com.bezirk.middleware.Bezirk;
 import com.bezirk.middleware.BezirkListener;
 import com.bezirk.middleware.addressing.DiscoveredZirk;
+import com.bezirk.middleware.addressing.Location;
 import com.bezirk.middleware.addressing.Pipe;
 import com.bezirk.middleware.addressing.PipePolicy;
+import com.bezirk.middleware.addressing.RecipientSelector;
 import com.bezirk.middleware.addressing.ZirkEndPoint;
 import com.bezirk.middleware.messages.Event;
-import com.bezirk.middleware.messages.Message;
-import com.bezirk.middleware.messages.Message.Flag;
+import com.bezirk.middleware.messages.MulticastStream;
+import com.bezirk.middleware.messages.ProtocolRole;
 import com.bezirk.middleware.messages.Stream;
 import com.bezirk.middleware.proxy.Factory;
 
@@ -26,43 +28,79 @@ public class Test {
     public Test() {
         senderBezirk = Factory.registerZirk("sender");
         receiverBezirk = Factory.registerZirk("receiver");
+
+        receiverBezirk.subscribe(new TestRole(), new ReceiverListener());
+        receiverBezirk.setLocation(new Location("test/location"));
     }
 
     public static void main(String[] args) {
         Test test = new Test();
-        test.sayHello();
+        test.send();
     }
 
-    public void sayHello() {
-        // Steps to publish an even over Bezirk:
-        // 1. set the event to be published
-        Event hello = new Event(Flag.NOTICE, "Hello World");
+    public void send() {
+        Event helloEvent = new TestEvent();
+        senderBezirk.sendEvent(helloEvent);
+        System.out.println("Sender Published: " + helloEvent.toJson());
 
-        // 2. publish "hello world" to all in the target address
-        senderBezirk.sendEvent(hello);
+        Stream helloStream = new TestStream();
+        // TODO: create data stream and send it
 
-        // sanity check: display the event that was just published
-        System.out.println("Sender Published: " + hello.toJson());
+        // TODO: send file stream
+    }
+
+    private static class TestEvent extends Event {
+        private static final String TOPIC = "Hello World";
+
+        public TestEvent() {
+            super(Flag.NOTICE, TOPIC);
+        }
+    }
+
+    private static class TestRole extends ProtocolRole {
+        public String getRoleName() {
+            return TestRole.class.getSimpleName();
+        }
+
+        public String getDescription() {
+            return "This role is simply used for testing";
+        }
+
+        public String[] getEventTopics() {
+            String[] topics = {TestEvent.TOPIC};
+            return topics;
+        }
+
+        public String[] getStreamTopics() {
+            return null;
+        }
+    }
+
+    private static class TestStream extends MulticastStream {
+        public TestStream() {
+            super(Flag.NOTICE, "Hello Stream", new RecipientSelector(new Location("test/location")));
+        }
     }
 
     private static class ReceiverListener implements BezirkListener {
         @Override
         public void receiveEvent(String topic, Event event, ZirkEndPoint sender) {
-            System.out.println(topic + " " + event.topic);
+            System.out.println("Received Event with topic: " + event.topic);
         }
 
         @Override
         public void receiveStream(String topic, Stream stream, short streamId, InputStream inputStream, ZirkEndPoint sender) {
+            // TODO: Receive data stream
         }
 
         @Override
         public void receiveStream(String topic, Stream stream, short streamId, File file, ZirkEndPoint sender) {
+            // TODO: Receive file stream
         }
 
         @Override
         public void streamStatus(short streamId, StreamStates status) {
         }
-
 
         @Override
         public void pipeStatus(Pipe pipe, PipeStates status) {
