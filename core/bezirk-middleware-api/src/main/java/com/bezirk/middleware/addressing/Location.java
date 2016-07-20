6a3ec05260c4765dd4690f14b22afd7e44eb2383
Michael Maass <mam1pi@bosch.com>
2016-07-20 11:04:29 -0400

    Removed license data from API

--- core/bezirk-middleware-api/src/main/java/com/bezirk/middleware/addressing/Location.java
+++ core/bezirk-middleware-api/src/main/java/com/bezirk/middleware/addressing/Location.java
@@ -1,20 +1,3 @@
-/**
- * This file is part of Bezirk-Middleware-API.
- * <p>
- * Bezirk-Middleware-API is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * </p>
- * <p>
- * Bezirk-Middleware-API is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * </p>
- * You should have received a copy of the GNU General Public License
- * along with Bezirk-Middleware-API.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.bezirk.middleware.addressing;
 
 import com.bezirk.middleware.BezirkListener;
