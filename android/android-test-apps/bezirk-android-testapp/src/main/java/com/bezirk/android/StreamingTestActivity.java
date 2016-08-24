556ce687daa59544fbf74781dbf3760855309390
Rishabh <rishabhsgulati@gmail.com>
2016-08-24 11:36:42 -0400

    adding streaming activities to unified android testapp. Removing subscriber and publisher projects from android-test-apps to reduce build time.

---
+++ android/android-test-apps/bezirk-android-testapp/src/main/java/com/bezirk/android/StreamingTestActivity.java
@@ -0,0 +1,26 @@
+package com.bezirk.android;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.bezirk.android.bezirkAsALibrary.R;
+
+public class StreamingTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_streaming_test);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+    }
+
+}
