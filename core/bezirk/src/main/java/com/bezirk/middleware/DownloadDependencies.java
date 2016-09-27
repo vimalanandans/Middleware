902dddbd8c9a1cc2268bf35b38cd6fe3bbdd599e
Michael Maass <mam1pi@bosch.com>
2016-09-27 13:02:07 -0400

    Fix various static analysis findings

--- core/bezirk/src/main/java/com/bezirk/middleware/DownloadDependencies.java
+++ core/bezirk/src/main/java/com/bezirk/middleware/DownloadDependencies.java
@@ -1,17 +1,9 @@
 package com.bezirk.middleware;
 
 /**
- * Created by snn8kor on 9/16/2016.
- */
-/**
  * Empty class that is used to satisfy Gradle's distZip task's requirement for
  * a main class.
  */
-public class DownloadDependencies
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Do nothing.");
-    }
-
+public class DownloadDependencies {
+    public static void main(String[] args) { System.out.println("Do nothing."); }
 }
