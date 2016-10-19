ea8457ac76716f1c6db7957578848fc05bd36fb4
Rishabh <rishabhsgulati@gmail.com>
2016-10-19 18:01:09 -0400

    removing unused property files from data-storage module

--- core/data-storage/src/test/java/com/bezirk/middleware/core/datastorage/JavaPlatformPersistenceTestForConnection.java
+++ core/data-storage/src/test/java/com/bezirk/middleware/core/datastorage/JavaPlatformPersistenceTestForConnection.java
@@ -22,18 +22,22 @@
  */
 package com.bezirk.middleware.core.datastorage;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
 import static org.junit.Assert.assertNotNull;
 
 public class JavaPlatformPersistenceTestForConnection {
 
-//    @Test(expected = IOException.class)
+    //@Test(expected = IOException.class)
     public void testForNullConnection() throws Exception {
         String DBPath = null;
         DatabaseConnection connection = new DatabaseConnectionForJava(DBPath);
         connection.getPersistenceDAO();
     }
 
-//    @Test
+    //@Test
     public void testForValidConnection() throws Exception {
         String DBPath = "./";
         DatabaseConnection connection = new DatabaseConnectionForJava(DBPath);
