0e08e64b9d56e328026d804a9e19fdce3803d5c8
Rishabh <rishabhsgulati@gmail.com>
2016-10-19 17:42:00 -0400

    adding license

--- android/android-test-apps/bezirk-android/src/main/java/com/bezirk/middleware/android/ui/MainActivity.java
+++ android/android-test-apps/bezirk-android/src/main/java/com/bezirk/middleware/android/ui/MainActivity.java
@@ -1,3 +1,25 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Bezirk http://bezirk.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.bezirk.middleware.android.ui;
 
 import android.os.Bundle;
@@ -6,10 +28,6 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import com.bezirk.middleware.android.BezirkMiddleware;
-import com.bezirk.middleware.core.actions.StartServiceAction;
-import com.bezirk.middleware.core.actions.StopServiceAction;
-import com.bezirk.middleware.core.componentManager.LifeCycleCallbacks;
-import com.bezirk.middleware.core.componentManager.LifeCycleObservable;
 import com.bezirk.middleware.core.proxy.Config;
 
 import org.slf4j.Logger;
@@ -18,7 +36,7 @@ import org.slf4j.LoggerFactory;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity implements LifeCycleCallbacks{
+public class MainActivity extends AppCompatActivity{
     private static final Logger logger = LoggerFactory.getLogger(MainActivity.class);
     private Switch bezirkSwitch;
 
@@ -73,16 +91,5 @@ public class MainActivity extends AppCompatActivity implements LifeCycleCallback
                 });
             }
         }, 2000);
-    }
-
-
-    @Override
-    public void start(StartServiceAction startServiceAction) {
-
-    }
-
-    @Override
-    public void stop(StopServiceAction stopServiceAction) {
-
     }
 }
